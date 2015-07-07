189beaff9646e4b60b98071bb5ffdb405c6c4c8b
Christian Reinicke <culeche@gmail.com>
2015-07-07 10:40:14 +0200

    Iterate over incoherencies & repetitions. Load g matrices, and blending factors

--- blending_master.m
+++ blending_master.m
@@ -3,13 +3,31 @@
 % 
 % figure; imagesc(squeeze(p(:,1,:)));
 
+
+% Incoherency matrix
 in = load('Parameters/incoherency.mat');
 incoherency = in.incoherency; clear in
-
 [in,reps] = size(incoherency);
 
+
+% Blending parameters
+blend_pars = load('Parameters/Blending_pars.mat');
+b_tg = blend_pars.b_tg; clear blend_pars
+
 for iter = 1:in
+    
+    % Blending factor
+    b = b_tg(iter,1);
+    
     for rep = 1:reps
+        
+        % Indicate iteration numbers
+        sprintf('iter = %d / %d, rep = %d / %d',iter,size(b_tg,1),rep,reps)
+        
+        % Load g matrix
+        fileID = strcat('g-matrices/','in',num2str(iter*5),'-rep',num2str(rep),'.mat');
+        gamma = load(fileID);
+        g = gamma.g; 
         
     end
 end
