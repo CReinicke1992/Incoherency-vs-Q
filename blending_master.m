205349f601000a19ea6208267039a0dcc5140db8
Christian Reinicke <culeche@gmail.com>
2015-07-07 10:32:36 +0200

    Started writing a file to compute deblending quality for varying incoherency

--- blending_master.m
+++ blending_master.m
@@ -1,4 +1,15 @@
-data = load('Data/Data_red_Delphi_Bandlimited.mat');
-p = data.data_fil3d;
+% data = load('Data/Data_red_Delphi_Bandlimited.mat');
+% p = data.data_fil3d;
+% 
+% figure; imagesc(squeeze(p(:,1,:)));
 
-figure; imagesc(squeeze(p(:,1,:)));
+in = load('Parameters/incoherency.mat');
+incoherency = in.incoherency; clear in
+
+[in,reps] = size(incoherency);
+
+for iter = 1:in
+    for rep = 1:reps
+        
+    end
+end
